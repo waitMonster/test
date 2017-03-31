57b88fdaf73d76212044ea53104108c1aa7ff4fc
zhangyg <yaguang.zhang@qunar.com>
2017-03-31 21:50:23 +0800

    ######HTTPuntils

---
+++ fsTest/fs_UI.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import codecs
+import time
+import urllib2,urllib
+import requests
+from requests.cookies import RequestsCookieJar
+from selenium import webdriver
+import os
+import datetime
+from Data.devices import fs_datadevices,HttpUntils
+import json
+
+class UI:
+    def __init__(self,drive):
+        self.drive = drive
